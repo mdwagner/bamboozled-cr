fb5230f92b820e093f101ccaa2ac7d873ba08743
Michael Wagner <mdwranger@gmail.com>
2021-01-17 17:13:23 -0500

    feat: temp ignores for start of port

--- src/bamboozled.cr
+++ src/bamboozled.cr
@@ -1,6 +1,9 @@
-module Bamboozled
-  VERSION = "0.1.0"
+require "http/client"
+require "json"
+require "uri"
+require "bamboozled/**"
 
+module Bamboozled
   # Creates a standard client that will raise all errors it encounters
   def self.client(subdomain : String?, api_key : String?, http_options : Hash)
     Bamboozled::Base.new(subdomain, api_key, http_options)
---
+++ src/bamboozled/version.cr
@@ -0,0 +1,3 @@
+module Bamboozled
+  VERSION = "0.1.0"
+end
